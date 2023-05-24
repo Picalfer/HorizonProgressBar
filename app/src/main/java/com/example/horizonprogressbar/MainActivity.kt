0833a7adce7780aaafa8030fb591df6fac07403d
Arthur Kozhemyakin <trable228@icloud.com>
2023-05-24 10:29:50 +0300

    added top toolbar with buttons, onClickListeners

--- app/src/main/java/com/example/horizonprogressbar/MainActivity.kt
+++ app/src/main/java/com/example/horizonprogressbar/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.horizonprogressbar
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import com.google.android.material.appbar.MaterialToolbar
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textSuccess: TextView
     private lateinit var ivMain: ImageView
     private lateinit var iconRed: View
+    private lateinit var navMenu: MaterialToolbar
+    private lateinit var rootFrame: FrameLayout
     private var variety = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +36,8 @@ class MainActivity : AppCompatActivity() {
         textSuccess = findViewById(R.id.tvSuccess)
         ivMain = findViewById(R.id.iv_main)
         iconRed = findViewById(R.id.icon_red)
+        navMenu = findViewById(R.id.top_bar)
+        rootFrame = findViewById(R.id.root_frame)
 
         // todo testing xml features
         // setContentView(R.layout.test)
@@ -43,6 +49,32 @@ class MainActivity : AppCompatActivity() {
             .into(ivMain)
 
         progressBar.max = 100
+
+        navMenu.setNavigationOnClickListener {
+            Toast.makeText(this, "Someday there will be navigation...", Toast.LENGTH_SHORT).show()
+        }
+
+        navMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.fav -> {
+                    Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.search -> {
+                    changeTheme()
+                    Toast.makeText(this, "Dark theme", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.more -> {
+                    Toast.makeText(this, "More", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
 
         startProgress.setOnClickListener {
 
@@ -83,7 +115,11 @@ class MainActivity : AppCompatActivity() {
             textSuccess.text = currentSuccess.toString()
             progressBar.progress = currentProgress
             iconRed.rotation = currentRotation
-
         }
+    }
+
+    private fun changeTheme() {
+        // TODO:
+        // rootFrame.background = AppCompatResources.getDrawable(this, R.drawable.bg_red_gradient)
     }
 }
